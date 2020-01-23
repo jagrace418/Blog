61fc86f48db5b01510c10ebca97115f2d1ffc5f2
Jake Grace <jagrace418@gmail.com>
2020-01-22 18:28:37 -0600

    started user profile

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+	<div>
+		<div>
+			<div>
+				{{--			image here--}}
+			</div>
+			<div>
+				{{--			name here--}}
+			</div>
+		</div>
+	</div>
+@endsection
